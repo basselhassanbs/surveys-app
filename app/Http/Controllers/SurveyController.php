642917df49c43d3b03cfce91f127b4090a6f706b
bassel <user.name1995@hotmail.com>
2021-04-29 15:26:57 +0300

    SurveyController && api.php Updated

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Answer;
 use App\Models\Survey;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SurveyController extends Controller
 {
@@ -18,8 +19,9 @@ class SurveyController extends Controller
             'purpose' => 'required',
         ]);
 
-        Survey::create($data);
-        
+        $survey = Survey::create($data);
+        $survey->questions()->createMany(request()->questions);
+
         return ['message' => 'Survey Created!'];
     }
 
@@ -28,16 +30,27 @@ class SurveyController extends Controller
         return $survey;
     }
 
-    public function take(Survey $survey)
+    public function take()
     {
         $questions = request()->all();
         foreach ($questions as $question) {
             $data = [
                 'value' => $question['value'],
                 'question_id' => $question['question_id'],
-                'survey_id' => $survey->id
+                'survey_id' => $question['survey_id']
             ];
             Answer::create($data);
         }
+    }
+
+    public function answers($survey_id,$question_id,$value){
+        $answers = DB::table('answers')
+            ->where('survey_id',$survey_id)
+            ->where('question_id',$question_id)
+            ->where('value', $value)
+            ->select(DB::raw('count(*) as count'))
+            ->value('count');
+
+            return $answers;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -22,4 +22,6 @@ Route::get('/surveys', [\App\Http\Controllers\SurveyController::class,'index']);
 Route::post('/surveys', [\App\Http\Controllers\SurveyController::class, 'store']);
 Route::get('/surveys/{survey}', [\App\Http\Controllers\SurveyController::class,'show']);
 Route::post('/surveys/{survey}/questions', [\App\Http\Controllers\QuestionController::class, 'store']);
-Route::post('/surveys/{survey}/take', [\App\Http\Controllers\SurveyController::class, 'take']);
+Route::post('/surveys/take', [\App\Http\Controllers\SurveyController::class, 'take']);
+Route::get('/answers/{survey_id}/{question_id}/{value}', [\App\Http\Controllers\SurveyController::class,
+	'answers']);
